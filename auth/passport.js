74779304a9fcc652b277e6ddd98936f18de18d25
Tamal Patra <patra.tamal@gmail.com>
2020-09-01 21:25:13 +0530

    fix login session

--- auth/passport.js
+++ auth/passport.js
@@ -1,17 +1,18 @@
 const passport = require('passport');
 injector.add('passport', passport);
-const { getUserId } = require('./common');
 const userService = injector.resolve('userService');
 const app = injector.resolve('app');
 
 const enablePassportAuth = () => {
-  require('./google')();
-
-  passport.serializeUser((user, done) => done(null, getUserId(user)));
+  passport.serializeUser((user, done) => {
+    done(null, user.userId);
+  });
 
   passport.deserializeUser((userId, done) => {
-    userService.findOne({ userId }, (err, user) => done(err, user));
+    userService.findOne({ userId }, done);
   });
+
+  require('./google')();
 
   app.use(passport.initialize());
   app.use(passport.session());
