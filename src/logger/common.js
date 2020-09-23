b3beb80c8abb66f347aa998264d460f79cffd0b8
Tamal Patra <patra.tamal@gmail.com>
2020-09-23 20:59:11 +0530

    logger/common.ts

--- src/logger/common.js
+++
@@ -1,8 +0,0 @@
-const _prodEnvironments = ['prod', 'dr'];
-const _testEnvironments = ['test', 'uat', 'alfa', 'beta'];
-const _localEnvironment = 'local';
-module.exports = {
-  isProd: _prodEnvironments.includes(appConfig.environment),
-  isTest: _testEnvironments.includes(appConfig.environment),
-  isLocal: _localEnvironment === appConfig.environment,
-};
