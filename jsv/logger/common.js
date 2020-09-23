88ee8b40a312c095596895539681bc8da1a73d5c
Tamal Patra <patra.tamal@gmail.com>
2020-09-23 21:22:43 +0530

    removed backup `jsv`

--- jsv/logger/common.js
+++
@@ -1,8 +0,0 @@
-const _prodEnvironments = ['prod', 'dr'];
-const _testEnvironments = ['test', 'uat', 'alfa', 'beta'];
-const _localEnvironment = 'local';
-module.exports = {
-  isProd: _prodEnvironments.includes(process.appConfig.environment),
-  isTest: _testEnvironments.includes(process.appConfig.environment),
-  isLocal: _localEnvironment === process.appConfig.environment,
-};
--- jsv/services/userService.js
+++
@@ -1,6 +0,0 @@
-const UserSchema = require('../models/User');
-const mongoose = injector.resolve('mongoose');
-const userService = mongoose.model('User', UserSchema);
-injector.add('userService', userService);
-
-module.exports = userService;
